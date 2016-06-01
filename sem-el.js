b836ff8269f3e94e78af257c9bc41f909224f3c5
rektide <rektide@voodoowarez.com>
2016-05-31 23:34:51 -0400

    Start amakeProp.

--- sem-el.js
+++ sem-el.js
@@ -9,32 +9,33 @@ var
   _= require("lodash")
 
 module.exports.defaults = {
-	vocab: "http://schema.org",
+	vocab: "http://schema.org/",
 	tag: "div",
-	factories: React.DOM,
-	reactProps: 
+	factories: React.DOM
 }
 
 function makeClass(klass, factories, opts){
-	opt = opts || {}
+	opt= opts|| {}
 	var type= klass["@type"]
-	var name= type.toString()
+	var label= type.label
 
 	var statics = {
 		defaults: {
 			itemScope: true,
-			itemType: (opts.vocab || module.exports.vocab) + name
+			itemType: (opts.vocab || module.exports.vocab) + label
 		},
+		klass,
 		type
 	}
-	var klass= React.createClass({
-		displayName: name,
+
+	var reactKlass= React.createClass({
+		displayName: label,
 		statics,
 		render: function(props){
-			var sharedState = {
+			var sharedState= {
 				tag: this.state.tag
 			}
-			var merged = -.default({}, props, sharedState, klass.defaults, module.exports.defaults) 
+			var merged = _.default({}, props, sharedState, reactKlass.defaults, module.exports.defaults) 
 			if(merged.tag instanceof String){
 				merged.tag = merged.factories[merged.tag]
 			}
@@ -43,3 +44,41 @@ function makeClass(klass, factories, opts){
 	})
 	return factories
 }
+
+function makeProp(prop, factories, opts){
+	opts= opts|| {}
+	var label= prop.label
+	var domain= prop.domain
+	var range= prop.range
+
+	var statics = {
+		defaults: {
+			itemprop: label,
+			itemScope: true,
+			itemType: range
+		},
+		type
+	}
+	var reactProp= React.createClass({
+		displayName: label,
+		statics,
+		render: function(props){
+			var sharedState= {
+				tag: this.state.tag
+			}
+			var merged = _.default({}, props, sharedState, klass.defaults, module.exports.defaults) 
+			if(merged.tag instanceof String){
+				merged.tag = merged.factories[merged.tag]
+			}
+			return merged.tag(merged, this.children)
+		}
+	})
+	return factories
+
+}
+
+module.exports= function makeAll(){
+	
+}
+module.exports.makeClass= makeClass
+module.exports.makeProp= makeProp
